bea4e53d9ac8df8226d37ec9ffc3273f4921ff71
krishan <astrohari09@outlook.com>
2025-05-23 13:30:52 +0530

    Update clone.py

--- Clonify/plugins/tools/clone.py
+++ Clonify/plugins/tools/clone.py
@@ -102,8 +102,8 @@ async def clone_txt(client, message, _):
                 "name": bot.first_name,
                 "token": bot_token,
                 "username": bot.username,
-                "channel": "ProBotts",
-                "support": "ProBotGc",
+                "channel": "krishan_politicsss",
+                "support": "CUTIEE_BOT_SUPPORT",
                 "premium" : False,
                 "Date" : False,
             }
